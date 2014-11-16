263218f0c968c4c6c04316b090ecd9334c820455
Gianpaolo Macario <gmacario@gmail.com>
2014-11-16 12:37:23 +0100

    First cut at script to get Confluence backups from VM
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

---
+++ do-get-backups.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Purpose: Get all backup files
+
+set -e
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <vagrant_id>"
+    exit 1
+fi
+
+VAGRANT_ID=$1
+REMOTEDIR=/var/atlassian/application-data/confluence/temp
+
+vagrant ssh-config ${VAGRANT_ID} >tmp-sshconfig
+#ssh -F tmp-sshconfig atlassian ls -la ${REMOTEDIR}
+ssh -F tmp-sshconfig atlassian ls ${REMOTEDIR} >tmp-files
+for f in $(cat tmp-files); do
+    scp -F tmp-sshconfig atlassian:${REMOTEDIR}/${f} .
+done
+# TODO: rsync -avz xxx .
+
+# EOF
